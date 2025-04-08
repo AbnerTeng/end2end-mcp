0c13e4916d26f83fb515ff9958edd188d1efa7b8
AbnerTeng <abnerteng16@gmail.com>
2025-04-08 16:43:23 +0800

    [dev] add conversation cycle

--- src/shioaji-mcp-openai/client.py
+++ src/shioaji-mcp-openai/client.py
@@ -34,6 +34,7 @@ async def main():
             "args": ["run", "src/weather-server/tool.py"],
         },
     )
+    history_conversation = []
 
     async with shioaji_server as ss, weather_server as ws:
         trace_id = gen_trace_id()
@@ -42,7 +43,7 @@ async def main():
         with trace(workflow_name="Multi-MCP Server Workflow", trace_id=trace_id):
             agent = Agent(
                 name="MCP Assistant",
-                instructions=f"Use the tools to analyze csv and trade or show weather information",
+                instructions="Use the tools to analyze csv and trade or show weather information",
                 model="gpt-4o",
                 mcp_servers=[ss, ws],
             )
@@ -52,8 +53,10 @@ async def main():
                 if message.lower() == "exit":
                     print("Exiting conversation")
                     break
-
-                result = await Runner.run(starting_agent=agent, input=message)
+                history_conversation.append(f"User: {message}")
+                context = "\n".join(history_conversation)
+                result = await Runner.run(starting_agent=agent, input=context)
+                history_conversation.append(f"Agent: {result}")
 
                 print(f"Result: {result.final_output}")
 
